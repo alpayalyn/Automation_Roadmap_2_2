d06cc6d1be8b005e7341809c44effcb1e22f2580
Alpay Alın <alpay.aln@insider.local>
2021-10-09 11:13:30 +0300

    First

---
+++ main.py
@@ -0,0 +1,41 @@
+class Person():
+
+    def __init__(self, name, surname, age, country, city, skill_list):
+
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.country = country
+        self.city = city
+        self.skill_list = skill_list
+
+    def add_Skills(self):
+
+        yetenek = input(print("Would you like to add some skills?"))
+        self.skill_list.append(yetenek)
+        print(self.skill_list)
+
+    def Person_Info(self):
+
+        return self.name, self.surname, self.age, self.country, self.city, self.skill_list
+
+AbouthePerson = Person("Alpay", "Alin", "18", "Turkey", "Istanbul", ["Basketbol"])
+
+
+
+while True:
+
+    answer = input(print("Do you still want to add skill into the skills list? Y / N"))
+
+    if(answer == "Y"):
+
+        AbouthePerson.add_Skills()
+
+        lelo = AbouthePerson.Person_Info()
+        print(lelo)
+
+
+    else:
+
+        break
+
